377f4c8c73daefaebd89b0d8df8d5de0996be71e
sebastiencattoor <146442846+sebastiencattoor@users.noreply.github.com>
2023-12-14 16:46:15 +0100

    Add files via upload

---
+++ code/proof of concepts/interrupt/interrupt.ino
@@ -0,0 +1,25 @@
+const int buttonPin = 2;
+const int ledPin = 4;
+volatile int buttonState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, RISING);
+}
+
+void loop() {
+  // De hoofdcode hier kan andere taken uitvoeren terwijl het wacht op de interrupt
+}
+
+void buttonInterrupt() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("High");
+  } else {
+    digitalWrite(ledPin, LOW);
+    Serial.println("Low");
+  }
+}
